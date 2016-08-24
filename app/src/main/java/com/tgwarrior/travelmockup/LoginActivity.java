a356b410bae41983ef63cc52416065c56b5a59c7
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-08-24 16:38:24 +0700

    initial commit

---
+++ app/src/main/java/com/tgwarrior/travelmockup/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.tgwarrior.travelmockup;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @BindView(R.id.signInBtn) Button signInBtn;
+    @BindView(R.id.newHere) TextView newHere;
+    @BindView(R.id.forgotPassword) TextView forgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tgwarrior/travelmockup/SignUpActivity.java
@@ -0,0 +1,35 @@
+package com.tgwarrior.travelmockup;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends ActionBarActivity {
+
+    @BindView(R.id.editName) EditText editName;
+    @BindView(R.id.editEmail) EditText editEmail;
+    @BindView(R.id.editMobile) EditText editMobile;
+    @BindView(R.id.signUpBtn) Button signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
